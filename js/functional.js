51ea4d83c289eb51c5ee650a35f26b266ff19c15
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-19 19:23:10 +0600

    update

--- js/functional.js
+++ js/functional.js
@@ -7,21 +7,44 @@ function getInputValue(fieldId){
     inputField.value = '';
     return value;
 }
-
 function updateTotal(fieldId, amount){
     const totalTag = document.getElementById(fieldId);
     const previousTotalInText = totalTag.innerText;
     const previousTotal = parseFloat(previousTotalInText);
     const newTotal = previousTotal + amount;
     totalTag.innerText = newTotal;
+    return newTotal;
 }
 
+function updateBalance(amount,isAdding){
+    const balanceTag = document.getElementById('balance-total');
+    const balanceInText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalance;
+    if(isAdding == true){
+       newBalance = previousBalance + amount;
+    }
+    else{
+       newBalance = previousBalance - amount;
+    }
+   
+    balanceTag.innerText = newBalance;
+}
 
 
 document.getElementById('deposit-button').addEventListener('click',function(){
     const amount = getInputValue('deposit-input');
-    updateTotal('deposit-total');
+    updateTotal('deposit-total',amount);
+    updateBalance(amount,true);
 
+})
+
+// handle withdraw
+
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    const amount = getInputValue('withdraw-input');
+    updateTotal('withdraw-total',amount);
+    updateBalance(amount,false);
 })
 
 
